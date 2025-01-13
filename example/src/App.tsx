40da435a816d7d460a403af9208e4880cb4e6bee
Akshay Chouhan <akshay.chouhan@5exceptions.com>
2025-01-13 15:45:09 +0530

    Add Auth func in example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,19 +1,60 @@
 import { useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { multiply, clearAuthentication } from 'truvideo-react-turbo-core-sdk';
+import { View, StyleSheet, Button } from 'react-native';
+import {
+  isAuthenticated,
+  clearAuthentication,
+  generatePayload,
+  isAuthenticationExpired,
+  authenticate,
+  initAuthentication,
+} from 'truvideo-react-turbo-core-sdk';
+import QuickCrypto from 'react-native-quick-crypto';
 
-const result = multiply(3, 7);
 
 export default function App() {
   useEffect(() => {
-    //  authenticate('EPhPPsbv7e',await generatePayload(), '9lHCnkfeLl', '')
-    //   .then((resp) => {
-    //     console.log('result', resp);
-    //   })
-    //   .catch((error) => {
-    //     console.log('error', error);
-    //   });
+    authFunc();
   }, []);
+
+  const authFunc = async () => {
+    try {
+      const isAuth = await isAuthenticated();
+      console.log('isAuth', isAuth);
+      // Check if authentication token has expired
+      const isAuthExpired = await isAuthenticationExpired();
+      console.log('isAuthExpired', isAuthExpired);
+      //generate payload for authentication
+      const payload = await generatePayload();
+      const apiKey = 'YOUR-API-KEY';
+      const signature = 'YOUR-SECRET-KEY';
+      const sha256 = await toSha256String(signature, payload);
+      // Authenticate user
+      if (!isAuth || isAuthExpired) {
+        await authenticate(apiKey, payload, sha256, '');
+      }
+      // If user is authenticated successfully
+      const initAuth = await initAuthentication();
+      console.log('initAuth', initAuth);
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  const toSha256String = (signature: any, payload: any) => {
+    try {
+      // Create HMAC using 'sha256' and the provided signature as the key
+      const hmac = QuickCrypto.createHmac('sha256', signature);
+      // Update the HMAC with the payload
+      hmac.update(payload);
+      // Generate the HMAC digest and convert it to a hex string
+      const hash = hmac.digest('hex');
+      return hash;
+    } catch (error) {
+      console.error('Error generating SHA256 string:', error);
+      return '';
+    }
+  };
+
 
   const logOut = () => {
     clearAuthentication()
@@ -26,7 +67,6 @@ export default function App() {
   };
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
       <Button
         onPress={logOut}
         title="Logout..."
